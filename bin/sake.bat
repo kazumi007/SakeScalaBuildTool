ae0ed186151306f70625f72a7248f63322d9c387
kazumi007 <kazumi007@gmail.com>
2010-03-14 15:38:27 +0900

    added sake's class path.

--- bin/sake.bat
+++ bin/sake.bat
@@ -1,8 +1,26 @@
 ::#!
 @echo off
 rem Warning: untested!
-call scala 
+if "%SAKE_HOME%" == "" (
+  set _SAKE_TMP=%~dp0
+  set SAKE_HOME=%_SAKE_TMP%\..\
+)
+
+set SAKE_CLASSPATH=
+for %%f in ("%SAKE_HOME%\lib\*") do call :add_cpath "%%f"
+
+call scala -classpath %SAKE_CLASSPATH%
+
 goto :eof
+
 ::!#
 :load sake.scala
 build("%*")
+
+:add_cpath
+  if "%SAKE_CLASSPATH%"=="" (
+    set SAKE_CLASSPATH=%~1
+  ) else (
+    set SAKE_CLASSPATH=%SAKE_CLASSPATH%;%~1
+  )
+goto :eof
